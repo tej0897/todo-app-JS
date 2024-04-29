2d4829b56b1882d3d0d96edd51684af4b0b7e4ff
Tejas Kumar S <tej0897@gmail.com>
2024-04-30 01:46:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,14 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+}
+
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
 );
@@ -25,7 +33,7 @@ cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
 discardBtn.addEventListener("click", () => {
   confirmCloseDialog.close();
-  taskForm.classList.toggle("hidden");
+reset();
 });
 
 
@@ -44,15 +52,19 @@ taskForm.addEventListener("submit", (e) => {
     taskData.unshift(taskObj);
   }
 
-  taskData.forEach(({id, title, date, description}) => {
-      (tasksContainer.innerHTML += `
+  taskData.forEach(
+    ({ id, title, date, description }) => {
+        (tasksContainer.innerHTML += `
         <div class="task" id="${id}">
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
         </div>
       `)
     }
   );
+
+  reset()
 });
