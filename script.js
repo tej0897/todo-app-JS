433791aa3e5c68fe8e6f2f1d6493560e06692f97
Tejas Kumar S <tej0897@gmail.com>
2024-05-01 11:04:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,27 +25,32 @@ const addOrUpdateTask = () => {
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   }
-  
+
   updateTaskContainer()
   reset()
 };
 
 const updateTaskContainer = () => {
+  tasksContainer.innerHTML = "";
 
-  taskData.forEach(
+ taskData.forEach(
     ({ id, title, date, description }) => {
         (tasksContainer.innerHTML += `
         <div class="task" id="${id}">
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button> 
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button>
         </div>
       `)
     }
   );
 };
+
+const deleteTask = (buttonEl) => {
+
+}
 
 const reset = () => {
   titleInput.value = "";
