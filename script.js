cd3f7c58a4cca4ef55ed30b33200e4075d29cb9b
Tejas Kumar S <tej0897@gmail.com>
2024-05-02 08:54:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const updateTaskContainer = () => {
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
           <button onclick="editTask(this)" type="button" class="btn">Edit</button>
-          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
       `)
     }
@@ -58,7 +58,21 @@ const deleteTask = (buttonEl) => {
   taskData.splice(dataArrIndex, 1);
 }
 
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
 
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+  
+  
+}
 
 const reset = () => {
   titleInput.value = "";
